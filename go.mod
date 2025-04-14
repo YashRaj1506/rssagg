0fc12ed6d5918aa1a28885e2a12e83dc05dee599
YashRaj1506 <yashraj504300@gmail.com>
2025-04-15 01:41:42 +0530

    adding chi routing with http

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/YashRaj1506/rssagg
 
 go 1.23.4
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+
+)
 
 func main () {
 	fmt.Println("Hello world")
 
+	godotenv.Load(".env")
+
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("PORT:", portString)
